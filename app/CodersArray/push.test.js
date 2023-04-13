ebdaea5e0567e6dbf4123e46abdc318e644a731f
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-14 00:04:21 +0200

    Test length and link js with html

--- app/CodersArray/push.test.js
+++ app/CodersArray/push.test.js
@@ -1,6 +1,17 @@
 import CodersArray from "./CodersArray";
 
 describe("Given an object CodersArray", () => {
+  describe("When it recieves (1,7,9)", () => {
+    test("Then it should return 3", () => {
+      const inputNumbers = new CodersArray(1, 7, 9);
+      const expectedlength = 3;
+
+      const resultLength = inputNumbers.length;
+
+      expect(resultLength).toBe(expectedlength);
+    });
+  });
+
   describe("When it recieves 5", () => {
     test("Then it returns 1", () => {
       const inputNumber = 5;
